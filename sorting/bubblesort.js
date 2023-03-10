6b26d2adda2876fd386701ac06218135218b9e5f
Robert Quartey <37972474+robertquartey7@users.noreply.github.com>
2023-03-09 19:31:29 -0500

    .

---
+++ sorting/bubblesort.js
@@ -0,0 +1,15 @@
+function bubblesort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+console.log(bubblesort([37, 45, 29, 8]))
+parse
